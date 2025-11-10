96ed1b2c038084c0533606e58523a0472dc7aa87
tomwerner <tomtastisch@icloud.com>
2025-11-10 18:24:44 +0100

    Einbinden der Util-Klassen und des angel-projektes in ufo.

--- thi/i/ki/project/ufo/src/core/ufo_main.py
+++ thi/i/ki/project/ufo/src/core/ufo_main.py
@@ -1,9 +1,12 @@
 # thi/i/ki/project/ufo/src/core/ufo_main.py
 # Vorgabe wie angel: keine Funktionsdefinitionen
 from util.evaluation import read_input, eps
+from core.angel import angel
 
 eingabe: float = read_input("test eingabe (int): ", int)
 print(eingabe)
+a = angel(eingabe, eingabe, eingabe, eingabe)
+print(a)
 
 # Platzhalter-Ausgabe für den Start
 if __name__ == "__main__":
